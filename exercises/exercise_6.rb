f3438e945672bcac0008180b2d8a441e06c72299
Xihai Luo <xihai.luo01@gmail.com>
2022-01-04 16:21:46 +0000

    add exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Xihai", last_name: "Luo", hourly_rate: 80)
+@store2.employees.create(first_name: "Vincent", last_name: "Wang", hourly_rate: 60)
+@store2.employees.create(first_name: "Arda", last_name: "Inan", hourly_rate: 30)
+@store2.employees.create(first_name: "Iris", last_name: "Sit", hourly_rate: 10)
+
