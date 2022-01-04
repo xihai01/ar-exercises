e5512ee02e045d3381ec00d519bb1b51ad6db8d7
Xihai Luo <xihai.luo01@gmail.com>
2022-01-03 23:51:04 -0500

    add exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# extract store1 and store2
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.update(name: "gap")
+
+p Store.all
