9eb7c2469cc8e1922a6579c58974f095606fa394
Xihai Luo <xihai.luo01@gmail.com>
2022-01-04 16:09:38 +0000

    add exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,9 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# fetch total revenue of all stores
+puts Store.sum("annual_revenue")
+# fetch avg revenue for all stores
+puts Store.average("annual_revenue")
+# fetch # of stores generating $1mil or more in revenue
+puts Store.where("annual_revenue >= ?", 1000000).count
